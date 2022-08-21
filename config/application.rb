8add27a37d2be7696b9fe6224e3ce98b0c1e2b1c
moeko <inomatamoeko@gmail.com>
2022-08-21 13:34:42 +0900

    Add:言語の設定

--- config/application.rb
+++ config/application.rb
@@ -32,11 +32,16 @@ module MiniTheater
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
-
     # タイムゾーンの設定
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
+
+    # デフォルトのlocaleを日本語にする
+    config.i18n.default_locale = :ja
+    # localeファイルの読み込み設定
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
