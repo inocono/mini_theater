5bb5b14f5877bb38bbb06c163847d8f34031e2c8
moeko <inomatamoeko@gmail.com>
2022-08-21 13:22:39 +0900

    Add:タイムゾーンの設定

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,9 @@ module MiniTheater
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # タイムゾーンの設定
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
   end
 end
