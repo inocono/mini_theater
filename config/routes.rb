4306160bbb6086419757ca56a9ef419aa4b287d0
moeko <inomatamoeko@gmail.com>
2022-09-04 16:24:50 +0900

    Add:userコントローラの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'top/index'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'top#index'
 
